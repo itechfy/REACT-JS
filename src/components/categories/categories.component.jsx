b9efd1f7dec73d3408d65b8d0a387549ace11073
Hamza Shah - TechFy <anonymous.shah667@gmail.com>
2022-09-15 13:08:46 +0500

    15/09/2022

--- src/components/categories/categories.component.jsx
+++ src/components/categories/categories.component.jsx
@@ -2,6 +2,15 @@ import './categories.styles.scss';
 import CategoryItem from '../category/category.component';
 const Categories = ({categories})=>{
     return(
+        <div>
+            <div className='hero'>
+            <h1 className='landing-title'>Welcome to Nike</h1>
+            <p>Bringing you the elite fashion outlets to nail out there. </p>
+            <div style={{
+                backgroundImage:`url('../../assets/icons8-nike.svg')`
+            }}>
+            </div>
+            </div>
         <div className="categories-container">
   { 
   categories.map((category)=>(
@@ -10,6 +19,7 @@ const Categories = ({categories})=>{
   }
    
 
+    </div>
     </div>
     );
 }
