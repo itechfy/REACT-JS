3339a2f71dfb99bdc9588c9c27f8800844e9d748
Hamza Shah - TechFy <anonymous.shah667@gmail.com>
2022-09-28 18:10:56 +0500

    Added reducers for cart and User Context

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -12,7 +12,7 @@ const CartItem = ({ cartItem }) => {
       <div className="item-details">
         <span className="name">{name}</span>
         <span>
-          {quantity} X {price}
+          {quantity} x {price}
         </span>
       </div>
       <div className="remove-button" onClick={() => removeItemofCart(cartItem)}>
